4872ed95fbea6e37b1855ccc38f42a62d3663743
Enayat Afridi <inayat.pcdc@gmail.com>
2023-04-01 12:14:25 +0500

    feat: a little bit about one way binding...

--- frontend/mean-stack/src/app/app.component.ts
+++ frontend/mean-stack/src/app/app.component.ts
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 	title = 'Mean Stack';
+
+	nums = [2, 5]
+	sum = () => {
+		return this.nums[0] + this.nums[1]
+	}
+
+	red = "color: red"
+	green = "color: green"
+
+	message = ""
+	clickMe = (val: string) => {
+		console.log(val)
+		this.message = val
+	}
 }
