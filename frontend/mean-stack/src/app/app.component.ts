a3769576f3f4dd47bd1b3547eed72d52fe6dd987
Enayat Afridi <inayat.pcdc@gmail.com>
2023-04-01 13:50:07 +0500

    feat: using NgForOf directives...

--- frontend/mean-stack/src/app/app.component.ts
+++ frontend/mean-stack/src/app/app.component.ts
@@ -8,13 +8,16 @@ import { Component } from '@angular/core';
 export class AppComponent {
 	title = 'Mean Stack';
 
-	nums = [2, 5]
+	nums = [2, 5, 25, 53, 22]
 	sum = () => {
 		return this.nums[0] + this.nums[1]
 	}
 
 	red = "color: red"
 	green = "color: green"
+
+	bggreen = "background-color: green";
+	bgred = "background-color: red";
 
 	message = ""
 	clickMe = (val: string) => {
@@ -25,5 +28,12 @@ export class AppComponent {
 	show = true;
 	toggle = () => {
 		this.show = !this.show;
+	}
+
+	inc() {
+		this.nums.push(Math.floor(Math.random() * 100));
+	}
+	dec() {
+		this.nums.pop();
 	}
 }
