bd17226c8e76e03e1bec3a53a960f773a4681e13
Enayat Afridi <inayat.pcdc@gmail.com>
2023-04-01 12:46:04 +0500

    feat: practiced two way data binding...

--- frontend/mean-stack/src/app/app.module.ts
+++ frontend/mean-stack/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { AboutComponent } from './about/about.component';
@@ -10,7 +11,8 @@ import { AboutComponent } from './about/about.component';
     AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
